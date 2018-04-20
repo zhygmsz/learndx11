1a9e31b37a443b71cfc0b75247e774779b085fff
zhaohaoyang <652246564@qq.com>
2018-04-20 20:45:17 +0800

    添加ansi和wchar转换

--- Common/d3dUtil.h
+++ Common/d3dUtil.h
@@ -118,6 +118,68 @@ static inline std::string format(const char *fmt, ...)
 	return strRet;
 }
 
+static inline std::string WChar2Ansi(LPCWSTR pwszSrc)  
+{
+	int nLen = WideCharToMultiByte(CP_ACP, 0, pwszSrc, -1, NULL, 0, NULL, NULL);
+
+	if (nLen <= 0)
+	{
+		return std::string("");
+	}
+
+	char* pszDst = new char[nLen];
+	if (NULL == pszDst)
+	{
+		return std::string("");
+	}
+
+	WideCharToMultiByte(CP_ACP, 0, pwszSrc, -1, pszDst, nLen, NULL, NULL);
+	pszDst[nLen -1] = 0;
+
+	std::string strTemp(pszDst);
+	delete[] pszDst;
+
+	return strTemp;
+} 
+
+static inline std::string Wstr2Str(std::wstring &inputws)
+{
+	return WChar2Ansi(inputws.c_str());
+}
+
+static inline std::wstring Ansi2WChar(LPCSTR pszSrc, int nLen)
+{  
+	int nSize = MultiByteToWideChar(CP_ACP, 0, (LPCSTR)pszSrc, nLen, 0, 0);
+	if(nSize <= 0) return NULL;
+
+	WCHAR *pwszDst = new WCHAR[nSize + 1];
+	if(NULL == pwszDst)
+	{
+		return NULL;
+	}
+
+	MultiByteToWideChar(CP_ACP, 0, (LPCSTR)pszSrc, nLen, pwszDst, nSize);
+	pwszDst[nSize] = 0;
+
+	if(pwszDst[0] == 0xFEFF) // skip Oxfeff
+	{
+		for(int i = 0; i < nSize; i ++)
+		{
+			pwszDst[i] = pwszDst[i+1];
+		}
+	}
+
+	std::wstring wcharString(pwszDst);
+	delete pwszDst;
+
+	return wcharString;
+} 
+
+static inline std::wstring Str2Wstr(const std::string &s)
+{
+	return Ansi2WChar(s.c_str(), s.size());
+}
+
 // Order: left, right, bottom, top, near, far.
 void ExtractFrustumPlanes(XMFLOAT4 planes[6], CXMMATRIX M);
 
